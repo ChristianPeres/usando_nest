d3bb09aaaf572a705bac5ecdf591faffc7e37c03
CHRISTIAN <christianperesg@gmail.com>
2025-01-07 13:57:14 -0400

    Primeiro commit

--- src/usuario.controller.ts
+++ src/usuario.controller.ts
@@ -1,10 +1,12 @@
-/* eslint-disable prettier/prettier */
 import { Body, Controller, Post } from '@nestjs/common';
+import { usuarioRepository } from './usuarios.repository';
 
 @Controller('/usuarios')
 export class UsuarioController {
+  private usuarioRepository = new usuarioRepository
   @Post()
   async criaUsuario(@Body() dadosDoUsuario) {
+    this.usuarioRepository.salvar(dadosDoUsuario)
     return dadosDoUsuario;
   }
 }
---
+++ src/usuarios.repository.ts
@@ -0,0 +1,10 @@
+
+export class usuarioRepository {
+  private usuarios = [];
+
+  async salvar(usuario) {
+    this.usuarios.push(usuario);
+    console.log(this.usuarios);
+    
+  }
+}
